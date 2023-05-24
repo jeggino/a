3f65d8e759e4498588c6db8ba907be9819194da7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 12:10:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ deta = Deta(st.secrets["data_key"])
 db = deta.Base("vdfvd")
 
 
-db_content = db.fetch().items
+db_content = db.fetch(limit=None).items
 df = pd.DataFrame(db_content)
-st.write(db_content)
+st.write(len(df))
+st.write(df)
