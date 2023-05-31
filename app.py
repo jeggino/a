04c406b6cd4b3fe3d6b5513835dcc605af80ddb4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-31 10:16:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,48 @@
 import streamlit as st
-from deta import Deta
 import pandas as pd
+import pydeck as pdk
+import numpy as np
+import geopandas
 
 
-# Connect to Deta Base with your Data Key
-deta = Deta(st.secrets["data_key"])
-
-db = deta.Base("vdfvd")
+df = pd.read_csv("https://github.com/jeggino/eBird/blob/bd7982301ec646c4b2a0288560370db39d121f01/df_raw.csv")
 
 
-db_content = db.fetch(limit=None).items
-df = pd.DataFrame(db_content)
-st.write(len(df))
-st.write(df)
+
+gdf = geopandas.GeoDataFrame(
+    df, geometry=geopandas.points_from_xy(df.lng, df.lat), crs="EPSG:4326"
+)
+
+gdf_scatter = gdf.dissolve(by='subId' ,aggfunc=np.size)[["comName","geometry"]].reset_index()
+
+# Define a layer to display on a map
+
+layer = pdk.Layer(
+    "GridLayer", gdf, pickable=True, extruded=True, 
+    cell_size=5000, elevation_scale=20, get_position="geometry.coordinates",
+)
+
+ScatterplotLayer = pdk.Layer(
+    "ScatterplotLayer",
+    gdf_scatter,
+    pickable=True,
+    opacity=0.8,
+    stroked=True,
+    filled=True,
+    radius_scale=200,
+    radius_min_pixels=gdf_scatter.comName.min(),
+    radius_max_pixels=gdf_scatter.comName.max(),
+    line_width_min_pixels=1,
+    get_position="geometry.coordinates",
+    get_radius="comName",
+    get_fill_color=[255, 140, 0],
+    get_line_color=[0, 0, 0],
+)
+
+tooltip_dictionary = {'ScatterplotLayer': {"text": "Name location: {subId} \nCount: {comName}"},
+                      'IconLayer': {"text": "Name: {sciName} \nDatet: {date}"} }
+    
+# Render
+r = pdk.Deck(layers=[ScatterplotLayer], map_style=pdk.map_styles.LIGHT, tooltip=tooltip_dictionary["ScatterplotLayer"],)
+
+st.pydeck_chart(pydeck_obj=r, use_container_width=True)
