995e1d2576eff589d8c312a08111ee680471176a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-17 12:20:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 
 # Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.experimental_connection('pets_db', type='sql')
+conn = st.experimental_connection('ebird', type='sql')
 
 # Query and display the data you inserted
 pet_owners = conn.query('select * from df')
