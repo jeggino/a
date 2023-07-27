2a3f23232c4d51102612fa048de31b515baa1272
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-27 17:24:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,33 @@
 import streamlit as st
 import pandas as pd
 
-# Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.experimental_connection('pets_db', type='sql')
+# # Create the SQL connection to pets_db as specified in your secrets file.
+# conn = st.experimental_connection('pets_db', type='sql')
 
-# Insert some data with conn.session.
-with conn.session as s:
-    s.execute('CREATE TABLE IF NOT EXISTS Tommaso (person TEXT, pet TEXT);')
-    s.execute('DELETE FROM Tommaso;')
-    # pet_owners = {'filippo': 'insalata', 'barbara': 'cat', 'alex': 'puppy'}
-    # for k in pet_owners:
-    #     s.execute(
-    #         'INSERT INTO Tommaso (person, pet) VALUES (:owner, :pet);',
-    #         params=dict(owner=k, pet=pet_owners[k]),
-    #     )
-    s.commit()
+# # Insert some data with conn.session.
+# with conn.session as s:
+#     s.execute('CREATE TABLE IF NOT EXISTS Tommaso (person TEXT, pet TEXT);')
+#     s.execute('DELETE FROM Tommaso;')
+#     # pet_owners = {'filippo': 'insalata', 'barbara': 'cat', 'alex': 'puppy'}
+#     # for k in pet_owners:
+#     #     s.execute(
+#     #         'INSERT INTO Tommaso (person, pet) VALUES (:owner, :pet);',
+#     #         params=dict(owner=k, pet=pet_owners[k]),
+#     #     )
+#     s.commit()
 
-# Query and display the data you inserted
-pet_owners = conn.query('select * from Tommaso')
-st.dataframe(pet_owners)
-df_ebird = pd.read_csv("df_raw (2).csv")
+# # Query and display the data you inserted
+# pet_owners = conn.query('select * from Tommaso')
+# st.dataframe(pet_owners)
+
+conn = st.experimental_connection(
+    "local_db",
+    type="sql",
+    url="mysql://root:Platinum79@localhost:3306/ebird"
+)
+df_ebird = conn.query("select * from df")
 st.dataframe(df_ebird)
+# df_ebird = pd.read_csv("df_raw (2).csv")
+# st.dataframe(df_ebird)
 st.map(df_ebird.iloc[:30], size=20,latitude="lat", longitude="lng")
 
