6fe41a3ed50b83e0b0b59f9de4b270e40b9f1d0b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-28 17:23:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,26 @@ import streamlit as st
 import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
+import streamlit as st
+import mysql.connector
 
 
-engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
-                       .format(user="root",
-                               pw="Platinum79",
-                               db="ebird"))
 
-q1 = 'SELECT * FROM df'
-df1 = pd.read_sql(q1, engine)
+def view_todo():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="Platinum79",
+        database="ebird"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM df")
+    todo = cursor.fetchall()
+    conn.close()
+    return todo
 
+df1 = view_todo()
 st.dataframe(df1)
+
+
+
