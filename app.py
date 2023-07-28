39620aa9fb0657f1927f4f20b66807cad3105052
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-28 16:47:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,7 @@ import streamlit as st
 import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
-# # Database connection
-# conn = pymysql.connect(host="localhost", user="root", password="Platinum79", db="ebird")
 
-# df = pd.read_sql("SELECT * FROM df",con=conn)
-
-# # df = conn.query("select * from pet_owners")
-# st.dataframe(df)
 
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                        .format(user="root",
@@ -22,16 +16,5 @@ API_KEY = 'm37q4mkeq3fj'
 BACK = 30
 COUNTRIES = ['IT','NL','FR','ES','BE','DE']
 
-df_old = pd.read_sql_query("SELECT * FROM df",con=engine)[COLUMNS]
-
-# conn = st.experimental_connection(
-#     "local_db",
-#     type="sql",
-#     url="mysql://root:Platinum79@localhost:3306/ebird"
-# )
-# df_ebird = conn.query("select * from df")
-# st.dataframe(df_ebird)
-# # df_ebird = pd.read_csv("df_raw (2).csv")
-# # st.dataframe(df_ebird)
-# st.map(df_ebird.iloc[:30], size=20,latitude="lat", longitude="lng")
+# df_old = pd.read_sql_query("SELECT * FROM df",con=engine)[COLUMNS]
 
