f85364302450d894e11e5cf2044b74d5ff330cc8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 07:31:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -112,6 +112,22 @@ with elements("callbacks_retrieve_data"):
             color="warning"
         )
 
+import plotly.figure_factory as ff
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
 with elements("dashboard"):
 
     # You can create a draggable and resizable dashboard using
@@ -130,33 +146,19 @@ with elements("dashboard"):
     # as first parameter, plus additional properties you can find in the GitHub links below.
 
     with dashboard.Grid(layout):
-        mui.TextField(
-            label="My text input dashborad",
-            defaultValue="Small",
-            variant="standard",
-            size="small",
-            color="warning",
-            key="first_item"
-        )
+        # mui.TextField(
+        #     label="My text input dashborad",
+        #     defaultValue="Small",
+        #     variant="standard",
+        #     size="small",
+        #     color="warning",
+        #     key="first_item"
+        # )
 
-        import plotly.figure_factory as ff
-
-        # Add histogram data
-        x1 = np.random.randn(200) - 2
-        x2 = np.random.randn(200)
-        x3 = np.random.randn(200) + 2
         
-        # Group data together
-        hist_data = [x1, x2, x3]
-        
-        group_labels = ['Group 1', 'Group 2', 'Group 3']
-        
-        # Create distplot with custom bin_size
-        fig = ff.create_distplot(
-                hist_data, group_labels, bin_size=[.1, .25, .5])
         
         # Plot!
-        st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig, use_container_width=True,key="first_item")
 
         # mui.Paper("First item", key="first_item")
         mui.Paper("Second item (cannot drag)", key="second_item")
