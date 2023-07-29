3fd2d99c65f357dce6b0d770aca3315b27f03820
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 06:46:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
 # streamlit_app.py
 import streamlit as st
+from sqlalchemy import create_engine
 import pandas as pd
-import pymysql
-# from sqlalchemy import create_engine
-
-
-conn = pymysql.connect(
-    # host="localhost",
-    user="root",
-    password="Platinum79",
-    database="ebird"
-)
-
-cursor = conn.cursor() 
-query = "SELECT * FROM df"
-cursor.execute(query)
-record = cursor.fetchone()
-
-if record:
-    st.write("GOOOODDDDD")
-else:
-    st.warning("Somthing wrong")
-
-
-# df1 = view_todo()
-# st.dataframe(df1)
+from ebird.api import get_observations
 
 
 
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                       .format(user="root",
+                               pw="Platinum79",
+                               db="ebird"))
+pd.read_sql()
+COLUMNS = ['comName', 'date', 'lat', 'lng', 'locId', 'sciName', 'subId']
+API_KEY = 'm37q4mkeq3fj'
+BACK = 30
+COUNTRIES = ['IT','NL','FR','ES','BE','DE']
+
+df_old = pd.read_sql("SELECT * FROM df",con=engine)[COLUMNS]
+
+records = get_observations(API_KEY, COUNTRIES,back=BACK)
+df_ebird = pd.DataFrame(records)
+df_ebird['date'] = df_ebird.obsDt.str.split(" ",expand=True)[0]
+df_ebird = df_ebird[COLUMNS]
+
+df_updated = pd.concat([df_ebird,df_old],axis=0)
+df_updated[['lat', 'lng']] = df_updated[['lat', 'lng']].astype("float")
+df_updated.drop_duplicates(inplace=True)
+df_updated.reset_index(drop=True,inplace=True)
+
+df_updated.to_sql(con=engine, name='df', if_exists='replace')
