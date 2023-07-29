988e40210586822f925148c8ac5cf0fe527acdbe
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 07:13:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,20 @@ with elements("properties"):
 
 with elements("style_mui_sx"):
 
+    # For Material UI elements, use the 'sx' property.
+    #
+    # <Box
+    #   sx={{
+    #     bgcolor: 'background.paper',
+    #     boxShadow: 1,
+    #     borderRadius: 2,
+    #     p: 2,
+    #     minWidth: 300,
+    #   }}
+    # >
+    #   Some text in a styled box
+    # </Box>
+
     mui.Box(
         "Some text in a styled box",
         sx={
