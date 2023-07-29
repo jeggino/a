12b9689dad2eade9072e5778f75f747d064cfb12
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 07:10:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ with elements("properties"):
 
 with elements("style_mui_sx"):
 
-    mui.Box(
+    with mui.Box(
         "Some text in a styled box",
         sx={
             "bgcolor": "background.paper",
@@ -57,15 +57,14 @@ with elements("style_mui_sx"):
             "borderRadius": 2,
             "p": 2,
             "minWidth": 300,
-        },
-         mui.TextField(
-            label="My text input",
-            # defaultValue="Type here",
-            # variant="standard",
-            defaultValue="Small",
-            variant="standard",
-            size="small",
-            color="warning"
-        )
+        }):
+             mui.TextField(
+                label="My text input",
+                # defaultValue="Type here",
+                # variant="standard",
+                defaultValue="Small",
+                variant="standard",
+                size="small",
+                color="warning"
+            )
         
-    )
