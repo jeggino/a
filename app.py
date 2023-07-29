fc2753e367a5be05cdce748b8bbfca6d2ac7a92a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 08:25:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -206,6 +206,80 @@ with elements("dashboard"):
 df = pd.read_csv("df_raw (2).csv")
 st.dataframe(df)
 
+df_plot = df_updated.groupby("sciName",as_index=False).size().loc[:10].to_dict('records')
+
+nivo.Bar(
+    data=df_plot,
+        indexBy="sciName",
+        margin={{ top: 50, right: 130, bottom: 50, left: 60 }},
+        padding={0.3},
+        valueScale={{ type: 'linear' }},
+        indexScale={{ type: 'band', round: true }},
+        colors={{ scheme: 'nivo' }},
+        defs={[
+            {
+                id: 'dots',
+                type: 'patternDots',
+                background: 'inherit',
+                color: '#38bcb2',
+                size: 4,
+                padding: 1,
+                stagger: true
+            },
+            {
+                id: 'lines',
+                type: 'patternLines',
+                background: 'inherit',
+                color: '#eed312',
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10
+            }
+        ]},
+        
+        borderColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    1.6
+                ]
+            ]
+        }},
+        axisTop={null},
+        axisRight={null},
+        axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'country',
+            legendPosition: 'middle',
+            legendOffset: 32
+        }},
+        axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'food',
+            legendPosition: 'middle',
+            legendOffset: -40
+        }},
+        labelSkipWidth={12},
+        labelSkipHeight={12},
+        labelTextColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    1.6
+                ]
+            ]
+        }},
+        
+        role="application",
+        ariaLabel="Nivo bar chart demo",
+        barAriaLabel={e=>e.id+": "+e.formattedValue+" in country: "+e.indexValue}
+)
 
 
     
