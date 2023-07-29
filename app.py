99b0d6d7eca1a75a81e520ed3098e2b617dfdd65
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 08:28:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -208,69 +208,70 @@ st.dataframe(df)
 
 df_plot = df.groupby("sciName",as_index=False).size().loc[:10].to_dict('records')
 
-nivo.Bar(
-    data=df_plot,
-        indexBy="sciName",
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }},
-        padding={0.3},
-        valueScale={{ type: 'linear' }},
-        indexScale={{ type: 'band', round: true }},
-        colors={{ scheme: 'nivo' }},
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: '#38bcb2',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: '#eed312',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-            }
-        ]},
-        
-      
-        axisTop={null},
-        axisRight={null},
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'country',
-            legendPosition: 'middle',
-            legendOffset: 32
-        }},
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'food',
-            legendPosition: 'middle',
-            legendOffset: -40
-        }},
-        labelSkipWidth={12},
-        labelSkipHeight={12},
-        labelTextColor={{
-            modifiers: [
-                [
-                    'darker',
-                    1.6
+with mui.Box(sx={"height": 500}):
+    nivo.Bar(
+        data=df_plot,
+            indexBy="sciName",
+            margin={{ top: 50, right: 130, bottom: 50, left: 60 }},
+            padding={0.3},
+            valueScale={{ type: 'linear' }},
+            indexScale={{ type: 'band', round: true }},
+            colors={{ scheme: 'nivo' }},
+            defs={[
+                {
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: '#38bcb2',
+                    size: 4,
+                    padding: 1,
+                    stagger: true
+                },
+                {
+                    id: 'lines',
+                    type: 'patternLines',
+                    background: 'inherit',
+                    color: '#eed312',
+                    rotation: -45,
+                    lineWidth: 6,
+                    spacing: 10
+                }
+            ]},
+            
+          
+            axisTop={null},
+            axisRight={null},
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'country',
+                legendPosition: 'middle',
+                legendOffset: 32
+            }},
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'food',
+                legendPosition: 'middle',
+                legendOffset: -40
+            }},
+            labelSkipWidth={12},
+            labelSkipHeight={12},
+            labelTextColor={{
+                modifiers: [
+                    [
+                        'darker',
+                        1.6
+                    ]
                 ]
-            ]
-        }},
+            }},
+            
+            role="application",
+            ariaLabel="Nivo bar chart demo",
+    )
+    
+    
         
-        role="application",
-        ariaLabel="Nivo bar chart demo",
-)
-
-
-    
-    
+        
