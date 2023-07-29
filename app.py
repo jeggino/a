d6ea69e15a45f67254d0fc4755e6198ebc0eb3aa
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 06:51:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ from streamlit_elements import elements, mui, html
 with elements("new_element"):
 
     mui.Typography("Hello world")
+
+
+with elements("multiple_children"):
+
+    mui.Button(
+        mui.icon.EmojiPeople,
+        mui.icon.DoubleArrow,
+        "Button with multiple children"
+    )
+
+
+    with mui.Button:
+        mui.icon.EmojiPeople()
+        mui.icon.DoubleArrow()
+        mui.Typography("Button with multiple children")
