ea07901eb4ec0e7e20471f67090f649604863043
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 07:40:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ with elements("dashboard"):
 
     layout = [
         # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
-        dashboard.Item("first_item", 0, 0, 2, 0, isDraggable=True, isResizable=True),
+        dashboard.Item("first_item", 0, 0, 2, 2, isDraggable=True, isResizable=True),
         dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
         dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
     ]
@@ -131,36 +131,28 @@ with elements("dashboard"):
 
     with dashboard.Grid(layout):
         
-        mui.TextField(
-            label="My text input dashborad",
-            defaultValue="Small",
-            variant="standard",
-            size="small",
-            color="warning",
-            key="first_item"
-        )
+        # mui.TextField(
+        #     label="My text input dashborad",
+        #     defaultValue="Small",
+        #     variant="standard",
+        #     size="small",
+        #     color="warning",
+        #     key="first_item"
+        # )
 
+        from streamlit_elements import nivo
 
-        # mui.Paper("First item", key="first_item")
-        mui.Paper("Second item (cannot drag)", key="second_item")
-        mui.Paper("Third item (cannot resize)", key="third_item")
-
-with elements("nivo_charts"):
-
-    # Streamlit Elements includes 45 dataviz components powered by Nivo.
-
-    from streamlit_elements import nivo
-
-    DATA = [
-        { "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
-        { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
-        { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
-        { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
-        { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },
-    ]
-
-    with mui.Box(sx={"height": 500}):
+        DATA = [
+            { "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
+            { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
+            { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
+            { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
+            { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },
+        ]
+    
+        # with mui.Box(sx={"height": 500}):
         nivo.Radar(
+            key="first_item",
             data=DATA,
             keys=[ "chardonay", "carmenere", "syrah" ],
             indexBy="taste",
@@ -204,4 +196,13 @@ with elements("nivo_charts"):
                 }
             }
         )
+
+
+        # mui.Paper("First item", key="first_item")
+        mui.Paper("Second item (cannot drag)", key="second_item")
+        mui.Paper("Third item (cannot resize)", key="third_item")
+
+
+
+    
     
