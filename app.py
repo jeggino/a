65706d24890d45683997a6c360267786d2761c74
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 07:29:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -139,6 +139,25 @@ with elements("dashboard"):
             key="first_item"
         )
 
+        import plotly.figure_factory as ff
+
+        # Add histogram data
+        x1 = np.random.randn(200) - 2
+        x2 = np.random.randn(200)
+        x3 = np.random.randn(200) + 2
+        
+        # Group data together
+        hist_data = [x1, x2, x3]
+        
+        group_labels = ['Group 1', 'Group 2', 'Group 3']
+        
+        # Create distplot with custom bin_size
+        fig = ff.create_distplot(
+                hist_data, group_labels, bin_size=[.1, .25, .5])
+        
+        # Plot!
+        st.plotly_chart(fig, use_container_width=True)
+
         # mui.Paper("First item", key="first_item")
         mui.Paper("Second item (cannot drag)", key="second_item")
         mui.Paper("Third item (cannot resize)", key="third_item")
