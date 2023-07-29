28b2b0b9eba95028edbcaffdd16c09a0a6ddb097
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 07:37:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -112,22 +112,6 @@ with elements("callbacks_retrieve_data"):
             color="warning"
         )
 
-import plotly.figure_factory as ff
-
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
 with elements("dashboard"):
 
     # You can create a draggable and resizable dashboard using
@@ -137,7 +121,7 @@ with elements("dashboard"):
 
     layout = [
         # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
-        dashboard.Item("first_item", 0, 0, 2, 2, isDraggable=True, isResizable=True),
+        dashboard.Item("first_item", 0, 0, 2, isDraggable=True, isResizable=True),
         dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
         dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
     ]
@@ -146,22 +130,78 @@ with elements("dashboard"):
     # as first parameter, plus additional properties you can find in the GitHub links below.
 
     with dashboard.Grid(layout):
-        # mui.TextField(
-        #     label="My text input dashborad",
-        #     defaultValue="Small",
-        #     variant="standard",
-        #     size="small",
-        #     color="warning",
-        #     key="first_item"
-        # )
+        
+        mui.TextField(
+            label="My text input dashborad",
+            defaultValue="Small",
+            variant="standard",
+            size="small",
+            color="warning",
+            key="first_item"
+        )
 
-        
-        
-        # Plot!
-        st.plotly_chart(fig, use_container_width=True,key="first_item")
 
         # mui.Paper("First item", key="first_item")
         mui.Paper("Second item (cannot drag)", key="second_item")
         mui.Paper("Third item (cannot resize)", key="third_item")
 
+with elements("nivo_charts"):
+
+    # Streamlit Elements includes 45 dataviz components powered by Nivo.
+
+    from streamlit_elements import nivo
+
+    DATA = [
+        { "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
+        { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
+        { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
+        { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
+        { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },
+    ]
+
+    with mui.Box(sx={"height": 500}):
+        nivo.Radar(
+            data=DATA,
+            keys=[ "chardonay", "carmenere", "syrah" ],
+            indexBy="taste",
+            valueFormat=">-.2f",
+            margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
+            borderColor={ "from": "color" },
+            gridLabelOffset=36,
+            dotSize=10,
+            dotColor={ "theme": "background" },
+            dotBorderWidth=2,
+            motionConfig="wobbly",
+            legends=[
+                {
+                    "anchor": "top-left",
+                    "direction": "column",
+                    "translateX": -50,
+                    "translateY": -40,
+                    "itemWidth": 80,
+                    "itemHeight": 20,
+                    "itemTextColor": "#999",
+                    "symbolSize": 12,
+                    "symbolShape": "circle",
+                    "effects": [
+                        {
+                            "on": "hover",
+                            "style": {
+                                "itemTextColor": "#000"
+                            }
+                        }
+                    ]
+                }
+            ],
+            theme={
+                "background": "#FFFFFF",
+                "textColor": "#31333F",
+                "tooltip": {
+                    "container": {
+                        "background": "#FFFFFF",
+                        "color": "#31333F",
+                    }
+                }
+            }
+        )
     
