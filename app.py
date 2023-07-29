47c8c0a86ac63b96be5e79a281b24cdc56d10dfd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-29 08:31:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ with elements("bar_chart"):
         nivo.Bar(
             data=df_plot,
                 indexBy="sciName",
-                margin={{ "top": 50, "right": 130, "bottom": 50, "left": 60 }},
+                margin={ "top": 50, "right": 130, "bottom": 50, "left": 60 },
                 padding={0.3},
                 valueScale={{ "type": 'linear' }},
                 indexScale={{ "type": 'band', "round": true }},
