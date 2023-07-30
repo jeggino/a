cc2851d91c12a18e543c7c01768fb5680aefd4fd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-30 12:17:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import streamlit as st
 conn = st.experimental_connection('pets_db', type='sql')
 
 st.write("ok")
+
+# Query and display the data you inserted
+pet_owners = conn.query('select * from df')
+st.dataframe(pet_owners)
