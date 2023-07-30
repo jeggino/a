69dfda98c3050cdf22007b156913c650d7104f04
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-30 12:43:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 import pymysql
+from sqlalchemy import create_engine
 
 # Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.experimental_connection('pets_db', type='sql')
+conn = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root",pw="Platinum79",db="ebird"))
 st.write("ok")
 
 df = pd.read_sql_query("SELECT * FROM df",con=conn)
