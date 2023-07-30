85285c2066283e8907e2b69e918a9a78719d9294
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-30 18:53:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 
 URL = "http://fasteri.com/list/2/short-names-of-countries-and-iso-3166-codes"
 API_KEY = 'm37q4mkeq3fj'
-BACK = st.number_input("Number of days back", min_value=0, max_value=30, value=1, step=1,  label_visibility="visible")
+BACK = st.sidebar.number_input("Number of days back", min_value=1, max_value=30, value=1, step=1,  label_visibility="visible")
 COLUMNS = ['comName', 'date', 'lat', 'lng', 'locId', 'sciName', 'subId']
 
 
@@ -15,15 +15,21 @@ list_ = {}
 for index, column in df_code.iterrows():
     list_[column["Country name"]] = column["ISO 3166 code"]
 
-COUNTRIES = st.selectbox("Select one o more countries", df_code["Country name"], placeholder="Choose an option")
+COUNTRIES = st.sidebar.selectbox("Select one o more countries", df_code["Country name"], placeholder="Choose an option")
 records = get_observations(API_KEY, list_[COUNTRIES],back=BACK)
 
 try:
     df_ebird = pd.DataFrame(records)
     df_ebird['date'] = df_ebird.obsDt.str.split(" ",expand=True)[0]
     df_ebird = df_ebird[COLUMNS]
-    
-    st.dataframe(df_ebird)
+
+    col1, col2 = st.columns(3)
+
+    with col1:
+        st.dataframe(df_ebird)
+
+    with col2:
+        st.map(data=df_ebird, latitude="lat", longitude="lng", color=None, size=None, zoom=None, use_container_width=True)
 
 except:
     st.error('Sorry, no data', icon="🚨")
