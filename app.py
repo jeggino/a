dcf3dda05b79c397adc14185f692088ccce656f4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-30 12:31:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
+import pymysql
+
 # Create the SQL connection to pets_db as specified in your secrets file.
 conn = st.experimental_connection('pets_db', type='sql')
-cur = conn.cursor()
 st.write("ok")
 
-df = pd.read_sql("SELECT * FROM df",con=cur)
+df = pd.read_sql("SELECT * FROM df",con=conn)
 st.dataframe(df)
