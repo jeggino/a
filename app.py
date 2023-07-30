4aa9a31bb7c809b9cf6b050a823aa440f2ebc60f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-30 12:18:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ conn = st.experimental_connection('pets_db', type='sql')
 
 st.write("ok")
 
+# Insert some data with conn.session.
+with conn.session as s:
+    s.execute('CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT);')
+    s.execute('DELETE FROM pet_owners;')
+    pet_owners = {'jerry': 'fish', 'barbara': 'cat', 'alex': 'puppy'}
+    for k in pet_owners:
+        s.execute(
+            'INSERT INTO pet_owners (person, pet) VALUES (:owner, :pet);',
+            params=dict(owner=k, pet=pet_owners[k])
+        )
+    s.commit()
+
 # Query and display the data you inserted
-pet_owners = conn.query('select * from df')
+pet_owners = conn.query('select * from pet_owners')
 st.dataframe(pet_owners)
