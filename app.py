2df264e4eaba31e800d6070d29fc7170a0b6b46a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-07-31 18:29:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,29 @@ try:
         )
 
         map2.pydeck_chart(pydeck_obj=deck, use_container_width=True)
+
+        #---
+        layer = pdk.Layer(
+            "GridLayer",
+            df,
+            pickable=True,
+            extruded=True,
+            cell_size=200,
+            elevation_scale=20,
+            get_position="geometry.coordinates",
+        )
         
+        view_state = pdk.ViewState(latitude=37.7749295, longitude=-122.4194155, zoom=11, bearing=0, pitch=45)
+        
+        # Render
+        r = pdk.Deck(
+            layers=[layer],
+            initial_view_state=view_state,
+            tooltip={"text": "{position}\nCount: {count}"},
+        )
+
+        map3.pydeck_chart(pydeck_obj=3, use_container_width=True)
+
                     
 except:
     st.error('Sorry, no data', icon="🚨")
